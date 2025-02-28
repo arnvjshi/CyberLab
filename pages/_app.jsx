a653c51401c919bf745940340659cc1e8d511cc9
Arnav Joshi <arnvjshi@gmail.com>
2025-02-28 15:26:28 +0530

    favicon added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -18,8 +18,7 @@ function MyApp({ Component, pageProps, router }) {
 
   return (
     <div className="app-container">
-      <title>CyberLab</title>
-      <link rel="icon" href="../public/favicon.png" />
+      <title>CyberLab</title><head><link rel="icon" href="../public/favicon.png" /></head>
       <div className="flex min-h-screen bg-black">
         <Sidebar />
         <div className="flex-1 flex flex-col">
