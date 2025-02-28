5ee1cfe0e2045c555f980cc9e545e30c115b0176
Arnav Joshi <arnvjshi@gmail.com>
2025-02-28 15:06:57 +0530

    upp

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'CyberLab',
-  description: 'Reserve A Classroom',
-  generator: 'Arnav Joshi',
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
